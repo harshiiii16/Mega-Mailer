8b7e2a7fc2832220a90874a2416fe7fd4e71fb48
Harshita <gautamharshita16@gmail.com>
2025-04-09 23:20:54 +0530

    added jwt files

---
+++ src/main/java/com/mainproject/useraccount/entity/FileInfo.java
@@ -0,0 +1,16 @@
+package com.mainproject.useraccount.entity;
+
+
+
+public class FileInfo {
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
