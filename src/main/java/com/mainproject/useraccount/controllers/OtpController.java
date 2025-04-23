dcb9e6da6384086a8f01f1f841d9a8685751598d
Harshita <gautamharshita16@gmail.com>
2025-04-23 22:00:29 +0530

    added otp and premium controller files

---
+++ src/main/java/com/mainproject/useraccount/controllers/OtpController.java
@@ -0,0 +1,72 @@
+package com.mainproject.useraccount.controllers;
+
+import com.mainproject.useraccount.entity.Otp;
+import com.mainproject.useraccount.entity.UserAuthentication;
+import com.mainproject.useraccount.services.OtpMailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins= "*")
+public class OtpController {
+
+    @Autowired
+    private OtpMailService otpMailService;
+
+@GetMapping("/welcome")
+public String welcome(){
+    return "Hello world";
+}
+
+    @PostMapping("/user/generateOtp")
+    public ResponseEntity<?> generateOtp(@RequestBody UserAuthentication details) {
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(this.otpMailService.signUp(details));
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An Error Occurred");
+        }
+    }
+
+    @PostMapping("/user/validate")
+    public ResponseEntity<?> validateOtp(@RequestBody Otp givenOtpDetails) {
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(this.otpMailService.validateSignUp(givenOtpDetails));
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An Error Occurred");
+        }
+    }
+
+
+    @PostMapping("/user/forgot")
+    public ResponseEntity<?> forgotPass(@RequestBody UserAuthentication forgotdetails)
+    {try{
+        return ResponseEntity.status(HttpStatus.OK).body( this.otpMailService.forgotPass(forgotdetails));
+    }
+    catch (Exception e)
+    {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An Error Occurred");
+    }
+    }
+
+    @PostMapping("/user/forgotValidate")
+    public ResponseEntity<?> validateForgotOtp(@RequestBody Otp forgotPassDetails){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body( this.otpMailService.validateForgotOtp(forgotPassDetails));
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An Error Occurred");
+        }
+    }
+
+}
+
+
+
---
+++ src/main/java/com/mainproject/useraccount/controllers/PremiumController.java
@@ -0,0 +1,46 @@
+package com.mainproject.useraccount.controllers;
+
+import com.mainproject.useraccount.configuration.JwtTokenUtil;
+import com.mainproject.useraccount.services.PremiumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins="*")
+public class PremiumController {
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private PremiumService premiumService;
+
+
+    @GetMapping("/premium")
+    public ResponseEntity<?> getPremium() {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication()
+                .getPrincipal();
+        String userName=userDetails.getUsername();
+        this.premiumService.premium(userName.toLowerCase());
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body("Premium account active");
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An Error Occurred");
+        }
+
+    }
+
+    @GetMapping("/checkPremium")
+    public Boolean checkPremium() {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication()
+                .getPrincipal();
+        String userName=userDetails.getUsername();
+        return this.premiumService.check(userName.toLowerCase());
+    }
+}
