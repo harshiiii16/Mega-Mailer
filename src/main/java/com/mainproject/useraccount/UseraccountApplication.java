74fa8acf5515dbc9f504867bb175861836abb9da
Harshita <gautamharshita16@gmail.com>
2025-04-25 17:31:02 +0530

    added useraccountapplication file

---
+++ src/main/java/com/mainproject/useraccount/UseraccountApplication.java
@@ -0,0 +1,66 @@
+package com.mainproject.useraccount;
+
+import com.mainproject.useraccount.services.FileStorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@SpringBootApplication
+@EnableAsync
+public class UseraccountApplication implements CommandLineRunner {
+	@Resource
+	FileStorageService storageService;
+
+
+	@Bean("threadPoolTaskExecutor")
+	public TaskExecutor getAsyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(20);
+		executor.setMaxPoolSize(1000);
+		executor.setQueueCapacity(1000);
+		executor.setWaitForTasksToCompleteOnShutdown(true);
+		executor.setThreadNamePrefix("Async-");
+		executor.initialize();
+		return executor;
+	}
+
+	public static void main(String[] args) {
+		try {
+			SpringApplication.run(UseraccountApplication.class, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS").allowedHeaders("*");
+			}
+		};
+	}
+
+	@Override
+	public void run(String... arg) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
+	}
+}
+
